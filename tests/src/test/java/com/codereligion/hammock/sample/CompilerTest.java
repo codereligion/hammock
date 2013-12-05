aeb78d56b5950c1d1d28b0c260ee8ed548018b04
Willi Schönborn <w.schoenborn@gmail.com>
2013-12-05 01:11:13 +0100

    Moved from gradle to maven

--- tests/src/test/java/com/codereligion/hammock/sample/CompilerTest.java
+++ tests/src/test/java/com/codereligion/hammock/sample/CompilerTest.java
@@ -2,17 +2,13 @@ package com.codereligion.hammock.sample;
 
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-import static com.google.common.collect.FluentIterable.*;
-import static org.hamcrest.MatcherAssert.*;
+import static com.codereligion.hammock.sample.Member_.*;
+import static com.google.common.collect.FluentIterable.from;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
@@ -31,9 +27,9 @@ public class CompilerTest {
         bob.setHappy(false);
 
         final FluentIterable<Member> members = from(ImmutableList.of(alice, bob));
-        
-        assertThat(((FluentIterable<Member>) members).filter(Member_.isHappy()).toList(), hasSize(1));
-        assertThat(members.transform(Member_.getNickName()).toList(), is(Arrays.asList("Ali", "Bob")));
+
+        assertThat(members.filter(isHappy()).toList(), hasSize(1));
+        assertThat(members.transform(getNickName()).toList(), is(Arrays.asList("Ali", "Bob")));
     }
 
 }
