47b32c1dec52b4d677ddb2bd4a9833eaa1b4a158
Willi Schönborn <w.schoenborn@gmail.com>
2013-12-06 23:13:55 +0100

    Added support to check for illegal use on java.lang.Object methods

--- compiler/src/main/java/com/codereligion/hammock/compiler/MethodParser.java
+++ compiler/src/main/java/com/codereligion/hammock/compiler/MethodParser.java
@@ -7,6 +7,9 @@ import com.codereligion.hammock.compiler.model.Name;
 import com.codereligion.hammock.compiler.model.Type;
 import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.reflect.Invokable;
+import com.google.common.reflect.Parameter;
 
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
@@ -14,6 +17,7 @@ import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeKind;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Set;
 
@@ -25,13 +29,19 @@ public class MethodParser implements Parser {
         final Set<Modifier> modifiers = method.getModifiers();
         final List<? extends VariableElement> parameters = method.getParameters();
 
+        final boolean returnsVoid = method.getReturnType().getKind() == TypeKind.VOID;
+
+        if (returnsVoid) {
+            throw new UnsupportedUsageException(method, "Void methods are not supported");
+        }
+
         final boolean isStatic = modifiers.contains(Modifier.STATIC);
-        
+
         if (isStatic) {
             if (parameters.isEmpty()) {
                 throw new UnsupportedUsageException(method, "static few arguments");
             }
-            
+
             if (parameters.size() > 1) {
                 throw new UnsupportedUsageException(method, "too many arguments");
             }
@@ -43,11 +53,60 @@ public class MethodParser implements Parser {
             }
         }
 
-        final boolean returnsVoid = method.getReturnType().getKind() == TypeKind.VOID;
+        final boolean isObjectMethod = isObjectMethod(method, parameters);
         
-        if (returnsVoid) {
-            throw new UnsupportedUsageException(method, "Void methods are not supported");
+        if (isObjectMethod) {
+            throw new UnsupportedUsageException(method, "can't use Object methods");
         }
+    }
+
+    private boolean isObjectMethod(ExecutableElement method, List<? extends VariableElement> parameters) {
+        for (Method m : Object.class.getDeclaredMethods()) {
+            final Invokable<?, Object> invokable = Invokable.from(m);
+            final boolean isPrivate = invokable.isPrivate();
+
+            if (isPrivate) {
+                continue;
+            }
+
+            final boolean isNotSameName = !method.getSimpleName().toString().equals(invokable.getName());
+
+            if (isNotSameName) {
+                continue;
+            }
+
+            final boolean isDifferentNumberOfArguments = parameters.size() != invokable.getParameters().size();
+
+            if (isDifferentNumberOfArguments) {
+                continue;
+            }
+            
+            final ImmutableList<Parameter> invokableParameters = invokable.getParameters();
+            final boolean typesMatch = typesMatch(parameters, invokableParameters);
+            
+            if (typesMatch) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    private boolean typesMatch(List<? extends VariableElement> parameters, ImmutableList<Parameter> invokableParameters) {
+        for (int i = 0; i < parameters.size(); i++) {
+            final VariableElement left = parameters.get(i);
+            final Parameter right = invokableParameters.get(i);
+
+            final boolean typesMatch = left.asType().toString().equals(right.getClass().getName());
+            
+            if (typesMatch) {
+                continue;
+            }
+            
+            return false;
+        }
+        
+        return true;
     }
 
     @Override
@@ -58,9 +117,9 @@ public class MethodParser implements Parser {
         final FirstClass annotation = method.getAnnotation(FirstClass.class);
         final ClosureName name = new ClosureName(method.getSimpleName().toString());
         final boolean isStatic = method.getModifiers().contains(Modifier.STATIC);
-        
+
         final Name parameterType;
-        
+
         if (isStatic) {
             final VariableElement firstParameter = method.getParameters().get(0);
             parameterType = new Name(firstParameter.asType().toString());
@@ -69,7 +128,7 @@ public class MethodParser implements Parser {
         }
 
         final Closure closure;
-        
+
         if (method.getReturnType().getKind() == TypeKind.BOOLEAN) {
             closure = new Closure(name, parameterType, isStatic, annotation.nullsafe());
         } else {
@@ -81,5 +140,5 @@ public class MethodParser implements Parser {
         Preconditions.checkNotNull(type, "No type found for %s", typeElement);
         type.getClosures().add(closure);
     }
-    
+
 }
